5639be6362de09cab7652ef2df7a3ec1f4e8aa0f
Afrida Aesha <afrida660@gmail.com>
2019-03-11 01:41:04 +0600

    updated readme.md

--- app.js
+++ app.js
@@ -1,30 +1,16 @@
+'use strict';
+
 const hapi = require('hapi'); 
 const path = require('path');
-
 const vision = require('vision');
 const handlebars = require('handlebars');
-
 const mongoose = require('mongoose');
-
 const joi = require('joi');
 const auth = require('hapi-auth-basic');
-
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-//user authentication
-
-const user = {
-    name: 'aesha',
-    password: '1234'
-};
-
-const validate = async (request, username, password, h) => {
-    let isValid = username === user.name && password === user.password;
-
-    return { isValid: isValid, credentials: {name: user.name} };
-};
-
+//coonnect to db
 mongoose.connect('mongodb://localhost:27017/studentdb', { useNewUrlParser: true }, (err) => {
     if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log(`Error in DB connection : ${err}`)}
@@ -37,6 +23,18 @@ const StudentModel = mongoose.model('Student', {
     email: String,
     password: String
 });
+
+//user authentication
+const user = {
+    name: 'aesha',
+    password: '1234'
+};
+
+const validate = async (request, username, password, h) => {
+    let isValid = username === user.name && password === user.password;
+
+    return { isValid: isValid, credentials: {name: user.name} };
+};
 
 const server = hapi.server({
     host: 'localhost',
@@ -51,7 +49,7 @@ const init = async () => {
     
     server.route({
         method: 'GET',
-        path: '/',
+        path: '/login',
         handler: async (request, h) => {
             return h.view('index');
         }
@@ -95,7 +93,7 @@ const init = async () => {
             try {
                   let salt = bcrypt.genSaltSync(saltRounds);
                   request.payload.password = bcrypt.hashSync(request.payload.password, salt);
-                  
+
                   let student = new StudentModel(request.payload); //req body on hapi
                   let result = await student.save();
                   return h.response(result);
@@ -120,6 +118,22 @@ const init = async () => {
             }
         }
     });
+
+    //remove
+    server.route({
+        method: 'GET',
+        path: '/list',
+        handler: async (request, h) => {
+            try {
+                let student = await StudentModel.find().exec();
+                return h.view('list',{
+                    student: student 
+                });
+            } catch (error) {
+                return h.response(error).code(500);
+            }
+        }
+    })
 
     await server.start();
     console.log('Server running at:', server.info.uri);
---
+++ readme.md
@@ -0,0 +1,8 @@
+## Hapi.js CRUD
+
+**Features**:
+
+> user authentication check
+> register
+> login
+> validation
